61776a4f13feb39e34c1777b260087635509b5b9
andre <andregaspar_@hotmail.com>
2018-05-10 16:38:56 -0300

    exercicio 10

---
+++ Exercicio_10/src/service/PaisService.java
@@ -0,0 +1,34 @@
+package service;
+import java.util.ArrayList;
+
+import dao.PaisDao;
+import model.Cliente;
+import model.Pais;
+
+public class PaisService {
+	PaisDao dao = new PaisDao();
+	
+	public int criar(Pais pais) {
+		return dao.criar(pais);
+	}
+	
+	public void atualizar(Pais pais) {
+		 dao.atualizar(pais);
+	}
+	
+	public void excluir (int id) {
+		dao.excluir(id);
+	}
+	
+	public Pais carregar(int id) {
+		return dao.carregar(id);
+	}
+	
+	public ArrayList<Pais> listarPais(String chave) {
+		return dao.listarPais(chave);
+	}
+	
+	public ArrayList<Pais> listarPais() {
+		return dao.listarPais();
+	}
+}
