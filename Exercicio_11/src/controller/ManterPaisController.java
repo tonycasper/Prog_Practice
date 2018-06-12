9fd24f1d403aa79b7188e7688161d4e31fb4eaac
andre <andregaspar_@hotmail.com>
2018-06-12 01:19:52 -0300

    arrumando cagada

---
+++ Exercicio_11/src/controller/ManterPaisController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+/**
+ * Servlet implementation class ManterPaisController
+ */
+@WebServlet("/ManterPais.do")
+public class ManterPaisController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ManterPaisController() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pNome = request.getParameter("nome");
+		int pPopu = Integer.parseInt(request.getParameter("popu"));
+		double pArea = Double.parseDouble(request.getParameter("area"));
+		
+		//instanciar o java bean
+		Pais pais = new Pais();
+		pais.setNome(pNome);
+		pais.setPopu(pPopu);
+		pais.setArea(pArea);
+		
+		//instanciar a service
+		PaisService ps = new PaisService();
+		ps.criar(pais);
+		pais = ps.carregar(pais.getId());
+		
+		//enviar para o jsp
+		request.setAttribute("pais", pais);
+		
+		RequestDispatcher view = 
+		request.getRequestDispatcher("Pais.jsp");
+		view.forward(request, response);
+		
+	}
+
+}
---
+++ Exercicio_12/src/command/VisualizarPais.java
@@ -0,0 +1,45 @@
+package command;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+public class VisualizarPais implements Command {
+
+	@Override
+	public void executar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String pId = request.getParameter("id");
+		String pNome = request.getParameter("nome");
+		int pPopu = Integer.parseInt(request.getParameter("populacao"));
+		double pArea = Double.parseDouble(request.getParameter("area"));
+		int id = -1;
+		try {
+			id = Integer.parseInt(pId);
+		} catch (NumberFormatException e) {
+
+		}
+
+		Pais pais = new Pais();
+		pais.setId(id);
+		pais.setNome(pNome);
+		pais.setPopu(pPopu);
+		pais.setArea(pArea);
+		PaisService ps = new PaisService();
+
+		RequestDispatcher view = null;
+		
+		pais = ps.carregar(pais.getId());
+		request.setAttribute("pais", pais);
+		view = request.getRequestDispatcher("VisualizarPais.jsp");
+		
+		view.forward(request, response);
+	}
+}
